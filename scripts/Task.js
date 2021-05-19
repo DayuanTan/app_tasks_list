0a0a92df359460a84b565dd7121dc22832d6cd40
BigCircle <zhuceafter2015@163.com>
2021-05-18 21:17:32 -0400

    try DOM localStorage success

---
+++ scripts/Task.js
@@ -0,0 +1,12 @@
+
+export default class Task {
+
+    constructor(taskText, taskID, checked, order, date) {
+        this.taskText = taskText;
+        this.taskID = taskID;
+        this.checked = checked;
+        this.order = order;
+        this.date = date;
+        
+    }
+}
